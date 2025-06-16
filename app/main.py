1a8220ca1d7fd0b0962f3908bc93cf20d92c3627
minryu87 <ryumin87@gmail.com>
2025-06-16 14:52:50 +0900

    헬스 체크 라우터 추가: main.py에 health 모듈의 라우터를 포함시켰습니다.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api import site_a, site_b, task_c, sample
+from app.api import site_a, site_b, task_c, sample, health
 
 app = FastAPI()
 
@@ -7,3 +7,4 @@ app.include_router(site_a.router, prefix="/site-a")
 app.include_router(site_b.router, prefix="/site-b")
 app.include_router(task_c.router, prefix="/task-c")
 app.include_router(sample.router, prefix="/sample")
+app.include_router(health.router)
