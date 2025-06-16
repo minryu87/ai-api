da1ff866f6f651e6477031bb9cd0cded27d87396
minryu87 <ryumin87@gmail.com>
2025-06-16 14:51:41 +0900

    샘플 라우터 추가: main.py에 sample 모듈의 라우터를 "/sample" 경로로 포함시켰습니다.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.api import site_a, site_b, task_c
+from app.api import site_a, site_b, task_c, sample
 
 app = FastAPI()
 
 app.include_router(site_a.router, prefix="/site-a")
 app.include_router(site_b.router, prefix="/site-b")
 app.include_router(task_c.router, prefix="/task-c")
+app.include_router(sample.router, prefix="/sample")
